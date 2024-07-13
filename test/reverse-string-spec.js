c8244f5d4449655f77da54b6a4c582543e2280bf
dcsiona@gmail.com <dcsiona@gmail.com>
2024-07-13 10:23:29 -0400

    My first commit.

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,12 @@
 // Your code here
+const { expect } = require('chai');
+const reverseString  = require("../problems/reverse-string");
+
+
+describe("reverseString", () => {
+    it("should reverse the input string", () => {
+        expect(reverseString("fun")).to.equal("nuf");
+
+    });
+});
+ 
