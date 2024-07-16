4ce2e0a717ff38e024b801fcccec60905117921c
dcsiona@gmail.com <dcsiona@gmail.com>
2024-07-16 10:01:48 -0400

    another commit

--- problems/person.js
+++ problems/person.js
@@ -1,5 +1,45 @@
 class Person {
   // Your code here
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello() {
+    return `Hello, my name is ${this.name}`;
+  }
+
+  visit(otherPerson) {
+    return `${this.name} visited ${otherPerson.name}`;
+  }
+
+  switchVisit(otherPerson) {
+    return otherPerson.visit(this);
+  }
+
+  update(obj) {
+    if (typeof obj !== 'object' || obj === null) {
+      throw new TypeError('Input must be an object');
+    }
+    if (!('name' in obj) || !('age' in obj)) {
+      throw new TypeError('Object must have name and age properties');
+    }
+    this.name = obj.name;
+    this.age = obj.age;
+  }
+
+  tryUpdate(obj) {
+    try {
+      this.update(obj);
+      return true;
+    } catch(error) {
+      return false;
+    }
+  }
+
+  static greetAll(people) {
+    return people.map(person => person.sayHello());
+  }
 }
 
 module.exports = Person;
