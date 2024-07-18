5666f17162037078e9a5bb14afd14eb91598c0c6
dcsiona@gmail.com <dcsiona@gmail.com>
2024-07-18 04:16:05 -0400

    another commit

--- problems/triangle.js
+++ problems/triangle.js
@@ -1,1 +1,30 @@
 // Your code here
+class Triangle {
+    constructor(side1, side2, side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    getPerimeter() {
+        return 
+    }
+
+    hasValidSideLengths() {
+        if (condition) {
+            
+        } else if (condition) {
+            
+        }
+    }
+
+    validate() {
+        if (condition) {
+            
+        }
+    }
+
+    static getValidTriangles() {
+
+    }
+}
